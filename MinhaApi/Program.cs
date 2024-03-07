7f5e81397f7cc7d14b39fc9f9202a993ee4a474d
lgc2 <lgustavoccarvalho@gmail.com>
2024-03-07 20:43:04 -0300

    first commit

---
+++ MinhaApi/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () =>
+{
+    return Results.Ok("Hello World!");
+});
+
+app.MapGet("/name/{nome}", (string nome) =>
+{
+    return $"Hello {nome}!";
+});
+
+app.MapPost("/", (User user) =>
+{
+    return Results.Ok(user);
+});
+
+app.Run();
+
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+}
