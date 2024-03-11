acb238bc3927d73cbf48e75036891f43b1905ba6
lgc2 <lgustavoccarvalho@gmail.com>
2024-03-11 20:28:25 -0300

    feat(Todo): implements CRUD

--- Todo/Controllers/HomeController.cs
+++ Todo/Controllers/HomeController.cs
@@ -1,14 +1,76 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Todo.Data;
+using Todo.Models;
 
 namespace Todo.Controllers
 {
     [ApiController]
     public class HomeController : ControllerBase
     {
-        [HttpGet]
-        public string Get()
+        [HttpGet("/")]
+        // [Route("/")]
+        public IActionResult Get([FromServices] AppDbContext context)
+            => Ok(context.Todos.ToList());
+
+        [HttpGet("/{id:int}")]
+        public IActionResult GetById([FromRoute] int id, [FromServices] AppDbContext context)
         {
-            return "Hello World!!!";
+            var todo = context
+                .Todos
+                .AsNoTracking()
+                .FirstOrDefault(x => x.Id == id);
+
+            if (todo is null)
+                return NotFound();
+
+            return Ok(todo);
+        }
+
+        [HttpPost("/")]
+        public IActionResult Post([FromBody] TodoModel todo, [FromServices] AppDbContext context)
+        {
+            context.Todos.Add(todo);
+            context.SaveChanges();
+
+            return Created($"/{todo.Id}", todo);
+        }
+
+        [HttpPut("/{id:int}")]
+        public IActionResult Put([FromRoute] int id, [FromBody] TodoModel todo, [FromServices] AppDbContext context)
+        {
+            var model = context
+                .Todos
+                .AsNoTracking()
+                .FirstOrDefault(x => x.Id == id);
+
+            if (model is null)
+                return NotFound();
+
+            model.Title = todo.Title;
+            model.Done = todo.Done;
+
+            context.Todos.Update(model);
+            context.SaveChanges();
+
+            return Ok(model);
+        }
+
+        [HttpDelete("/{id:int}")]
+        public IActionResult Delete([FromRoute] int id, [FromServices] AppDbContext context)
+        {
+            var model = context
+                .Todos
+                .AsNoTracking()
+                .FirstOrDefault(x => x.Id == id);
+
+            if (model is null)
+                return NotFound();
+
+            context.Todos.Remove(model);
+            context.SaveChanges();
+
+            return Ok(model);
         }
     }
 }
