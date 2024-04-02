54c6f625a689a6911d340109838bbee972950a51
lgc2 <lgustavoccarvalho@gmail.com>
2024-04-01 21:02:04 -0300

    feat(Blog): implements api key

--- Blog/Configuration.cs
+++ Blog/Configuration.cs
@@ -3,6 +3,8 @@
     public static class Configuration
     {
         // Token - JWT (Json Web Token)
-        public static string JwtKey { get; set; } = "narssirvQEqfPeiHl0CcaA==";
+        public static string JwtKey = "narssirvQEqfPeiHl0CcaA==";
+        public static string ApiKeyName = "api_key";
+        public static string ApiKey = "curso_api_FDBGdeLsAkW7r9MpbmUPiQ==";
     }
 }
