8e9c72dcdc3d0e474a88f062a88232bf68057cb6
lgc2 <lgustavoccarvalho@gmail.com>
2024-04-03 20:44:18 -0300

    feat(Blog): add configurations on appsettings file and refactor Program.cs

--- Blog/Configuration.cs
+++ Blog/Configuration.cs
@@ -6,5 +6,14 @@
         public static string JwtKey = "narssirvQEqfPeiHl0CcaA==";
         public static string ApiKeyName = "api_key";
         public static string ApiKey = "curso_api_FDBGdeLsAkW7r9MpbmUPiQ==";
+        public static SmtpConfiguration Smtp = new();
+
+        public class SmtpConfiguration
+        {
+            public string Host { get; set; }
+            public int Port { get; set; } = 25;
+            public string UserName { get; set; }
+            public string Password { get; set; }
+        }
     }
 }
